3a8f1255c3f6b64178260aaabe570ed6a60b8912
laual <laurent.jucquois@me.com>
2017-06-08 22:00:17 +0200

    added ft_putnbr

--- c/42/j02/ex06/ft_putnbr.c
+++ c/42/j02/ex06/ft_putnbr.c
@@ -5,7 +5,8 @@ void	ft_putnbr(int nb);
 
 int	main(void)
 {
-    ft_putnbr(31687);
+    ft_putnbr(42);
+    ft_putchar('\n');
     return (0);
 }
 
@@ -16,12 +17,9 @@ void	ft_putchar(char c)
 
 void	ft_putnbr(int numbera)
 {
-    int numberb;
-    
-    while(numbera > 10)
+    if (numbera != 0)
     {
-	numberb = numbera % 10;
-	ft_putchar('0' + numberb);
 	ft_putnbr(numbera / 10);
+	ft_putchar('0' + (numbera % 10));
     }
 }
